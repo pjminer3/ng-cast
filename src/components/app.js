6f3734d83b78f6867d690c18f2b9ab6e7c5a4b9f
pjminer3 <patrickminer318@gmail.com>
2017-10-27 22:40:20 -0700

    Refactored

--- src/components/app.js
+++ src/components/app.js
@@ -4,29 +4,15 @@ angular.module('video-player')
   bindings: {},
   controller: function(youTube) {
     
-    //console.log('this is youtube!', youTube);
-    
     this.selectVideo = (video) => {
       this.currentVideo = video;
     };
     
     this.searchResults = (query) => {
-      // console.log('click worked');
-      // console.log(query);
-      var options = {
-        // part: 'snippet',
-        // q: query,
-        // max: 5,
-        // key: 'AIzaSyC7rUNBo3-0BeF7wvmV0VMjoqRf3EkiRHw'
-        // part: 'snippet'
-      };
-      console.log('is it called here?');
       youTube.getYouTube(query, this.resetVideos);
-      console.log('is it called after?');
     };
     
     this.resetVideos = (vidData) => {
-      console.log('got data!', vidData);
       this.currentVideo = vidData[0];
       this.videos = vidData;
     };
@@ -35,11 +21,10 @@ angular.module('video-player')
     
     this.videos = window.exampleVideoData;
     
+    // call empty search result upon app.controller loading
+    this.searchResults('');
+    
   },
   templateUrl: 'src/templates/app.html'
 });
 
-
-//<video-player><h5><em>videoPlayer</em> component goes here</h5></video-player>
-      
-// <video-list><h5><em>videoList</em> component goes here</h5></video-list>
